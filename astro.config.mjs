0281782fbc504849c40c75aadc786502fa79ecda
Olan Healy <olanhealy12@gmail.com>
2026-09-18 20:52:56 +0100

    case study + updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwind from '@astrojs/tailwind';
 // set site to your GitHub Pages URL and base to your repo name.
 // When using a custom domain (bithdev.ie), remove `base` and update `site`.
 export default defineConfig({
-  site: 'https://bithdev.github.io',
-  base: '/bithdev-site',
+  site: process.env.NODE_ENV === 'production' ? 'https://bithdev.github.io' : undefined,
+  base: process.env.NODE_ENV === 'production' ? '/bithdev-site' : '/',
   output: 'static',
   integrations: [tailwind()],
 });
