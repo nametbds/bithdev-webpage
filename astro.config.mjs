efac461a237f9428ad364b053db4fe01d839f164
Olan Healy <olanhealy12@gmail.com>
2026-09-21 19:27:32 +0100

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://nametbds.github.io',
+  site: 'https://nametbds.github.io/bithdev-webpage',
+  base: '/bithdev-webpage',
   output: 'static',
   integrations: [tailwind()],
 });
